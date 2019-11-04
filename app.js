3fafaf7ab6c58a09113822e39929454d85c8c349
Valeriia-Rez <Valeria09122016@gmail.com>
2019-11-04 23:52:50 +0200

    change app.js,rolling the dice

--- app.js
+++ app.js
@@ -9,18 +9,41 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+
 
 document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
 
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
+//var x = document.querySelector("#score-0").textContent;
 
 document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    //Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //Display the result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
+    //Update the round score IF the rolled number was NOT a 1
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
